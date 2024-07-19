3a57923e75a97ecd6f585135411e7c1336541a9c
mattasathvika <mattasathvika984@gmail.com>
2024-07-19 13:16:47 +0530

    feature 202 changed Application.java file

--- Application.java
+++ Application.java
@@ -10,3 +10,14 @@ public class CreateWebSource
       {
         System.out.println(
           "Usage:\n  CreateWebSource <webServiceURL> <userName> <password> <webSourceURL>"
+
+#feature 202 changed by subbu module
+ 2. Export all sources to show the full definition
+        ObjectOutput oo = adminPort.exportAll(
+          "source",
+          null,
+          "password",
+          credentials,
+          null,
+          "en"
+        );
